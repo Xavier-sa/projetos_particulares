0bc577f283eac01c3f8b5467eae18d73d2fccc05
W.Xavier <wellington222688@gmail.com>
2025-02-14 21:36:39 -0400

    editar o arquivo e deixar pronto para proxima aula segunda feira

---
+++ app(atual)/view/components/navbar.php
@@ -0,0 +1,8 @@
+<!-- navegação -->
+
+<header class="navbar-grid">
+        NAVBAR
+        <!-- <nav>
+            <img src="user.jpg" alt="imagem-usuario">
+        </nav> -->
+    </header>
---
+++ app(atual)/view/components/sidebar.php
@@ -0,0 +1,18 @@
+   <!-- lateral -->
+
+   <aside class="sidebar-grid">
+        SIDEBAR
+        <!-- <nav>
+            <img src="logo.jpg" alt="logo">
+            <ul>
+                <li>
+                    <a href="index.html">Home</a>
+                </li>
+                <li>Artigos</li>
+                <li>
+                    <a href="categorias.html">Categorias</a>
+                </li>
+                <li>Usuários</li>
+            </ul>
+        </nav> -->
+    </aside>
