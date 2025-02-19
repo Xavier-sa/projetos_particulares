fab4b0174ecd979ac8735c728ac39dd683717449
Xavier-sa <wellington222688@gmail.com>
2025-02-19 17:08:32 -0300

    aprendndo sobre listas e tuplas no python

---
+++ Bootcamp_Dio_suzano/meu/Etapa05/listas.py
@@ -0,0 +1,16 @@
+frutas = ["maça", "laranja", "uva", "pera"]
+
+fruta = frutas[-1]
+
+
+print(fruta)
+
+
+
+#lista dentro de lista chamas-se LIsta aninhada
+
+#metodos
+
+
+n = [n**2 if n > 6 else n for n in range(10) if n % 2 == 0] 
+print(n)
