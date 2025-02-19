59a54e79b1959c2c6640c991cc71b765ce2622f4
Xavier-sa <wellington222688@gmail.com>
2025-02-18 22:42:03 -0300

    inciiando bootcamo dio-suzano

---
+++ Bootcamp_Dio_suzano/quiz_fazer/fazerquis.md
@@ -0,0 +1,8 @@
+Pyhton esta disponivel para quais sistemas operacionasi?
+windows e linux
+apenas windos 
+nenhuima das alternativas 
+windows e mac
+python é uma linguagem multiplatafornm 
+
+
---
+++ Bootcamp_Dio_suzano/tipos_de_dados.py
@@ -0,0 +1,19 @@
+tipos = """tipos built-in
+
+        texto       - str 
+        númerico    - int, float, complex
+        sequencia   -list, tuple,range
+        mapa        -dict
+        coleção     -set, fronzenset
+        booleano    -bool
+        binario     -bytes, bytearray, memoryview
+
+
+
+
+            """
+
+
+print(tipos)
+
+print(11 + 10)
